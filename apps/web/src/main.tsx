9c553432ad129a355a367cc1a51213ce598ab9b9
Milad Nekofar <milad@nekofar.com>
2025-05-24 22:19:22 +0300

    feat(web): integrate `wagmi` and `react-query` providers
    enhance state management and API handling by adding `wagmi` for blockchain interactions and `react-query` for efficient data fetching.

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,16 +1,25 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
-import App from "./App.tsx";
+import { WagmiProvider } from "wagmi";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import { ThemeProvider } from "@/components/theme-provider";
 
+import App from "./App.tsx";
+import { config } from "./wagmi.ts";
+
 import "@repo/ui/index.css";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
-      <App />
+      <WagmiProvider config={config}>
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
+      </WagmiProvider>
     </ThemeProvider>
   </StrictMode>,
 );
