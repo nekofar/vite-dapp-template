1e938e1fb3982a981c82a1ab49d45f4e57be871d
Milad Nekofar <milad@nekofar.com>
2025-05-25 20:12:26 +0300

    chore(deps): update eslint config dependencies
    add `@eslint-react/eslint-plugin` and enhance configs for stricter and stylistic TypeScript checks. Improves code quality and enforces better linting practices.

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -9,11 +9,17 @@ import sonarjsPlugin from "eslint-plugin-sonarjs";
 import importXPlugin from "eslint-plugin-import-x";
 import { createTypeScriptImportResolver } from "eslint-import-resolver-typescript";
 import unusedImportsPlugin from "eslint-plugin-unused-imports";
+import eslintReactPlugin from "@eslint-react/eslint-plugin";
 
 export default tseslint.config(
   { ignores: ["dist", "src/hooks/contracts.ts"] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.strictTypeChecked,
+      ...tseslint.configs.stylisticTypeChecked,
+      eslintReactPlugin.configs["recommended-typescript"],
+    ],
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
